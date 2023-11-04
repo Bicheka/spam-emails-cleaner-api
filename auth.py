f5ff36bc207d8b64f5d2abb53d77b335c24f0fd2
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-04 17:47:22 -0400

    requirements file updated

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field
 
 class AuthDetails(BaseModel):
-    email: EmailStr = Field(..., description="Email address", example="user@example.com")
-    password: str = Field(..., description="Password", min_length=8)
+    email: EmailStr = Field(..., description="Email address")
+    password: str = Field(..., description="Password")
