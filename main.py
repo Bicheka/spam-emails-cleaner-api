c6d8b66d1fc0cf4542b1db5d0f315a3d17ffe2ad
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-03 23:18:04 -0400

    fast api added

--- main.py
+++ main.py
@@ -1,58 +1,15 @@
-import imaplib
-import email
-from email.header import decode_header
+from typing import Union
+from fastapi import FastAPI
 
 
 
-# Configuration
-EMAIL = input("What is your email: ")
-PASSWORD = input("What is your app-password: ")
-SPAM_FOLDER = "[Gmail]/Spam"  # The folder where spam emails are moved to
 
-def delete_spam_emails():
-    # Connect to the IMAP server (Gmail in this case)
-    mail = imaplib.IMAP4_SSL("imap.gmail.com")
-
-    result = None
-
-    # Log in to your email account
-    if EMAIL is not None and PASSWORD is not None:
-        mail.login(EMAIL, PASSWORD)
-        result, _ = mail.select(SPAM_FOLDER)
-    else:
-        if EMAIL is None and PASSWORD is None:
-            print("email and password are none")
-        elif EMAIL is None:
-            print("Email is none!")
-        elif PASSWORD is None:
-            print("Password is None!")
-    # Select the spam folder
     
-    if result!=None and result == "OK":
-        # Search for all emails in the spam folder
-        result, email_ids = mail.search(None, "ALL")
-        
-        if result == "OK":
-            email_id_list = email_ids[0].split()
-            for email_id in email_id_list:
-                # Mark the email as deleted
-                mail.store(email_id, '+FLAGS', '(\Deleted)')
-            
-            # Permanently remove deleted emails (Expunge)
-            mail.expunge()
-        else:
-            print("Error searching for emails:", result)
+app = FastAPI()
 
-        # Close the mailbox
-        mail.close()
-    else:
-        print("Error selecting folder:", result)
- 
+@app.post("/clear")
+async def login(data: dict):
+    email = data.get("email")
+    password = data.get("password")
 
-    # Logout
-    mail.logout()
-
-if __name__ == "__main__":
-    delete_spam_emails()
-
-
+    return {"email": email, "password": password}
