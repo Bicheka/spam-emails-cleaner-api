2415241921739e7a6173da59576de56c9ab6b279
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-04 17:30:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,3 @@ async def clear_spam_emails(data: AuthDetails):
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
