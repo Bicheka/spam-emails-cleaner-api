9d9420e7a8bf58faa18a0be1eb6ffed40f89d8c6
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-10 14:05:24 -0500

    Update user_controller.py
    response message updated

--- user/user_controller.py
+++ user/user_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from user.user_model import User, UserResponse
 import user.user_service as user_service
 router = APIRouter()
@@ -7,7 +7,18 @@ router = APIRouter()
 # Create a new user
 @router.post("/register", response_model=UserResponse)
 async def register_user(user: User):
-    return await user_service.create_user(user)
+    try:
+        await user_service.register_user(user)
+        return {
+                "status": "success",
+                "message": user.email + " registered successfully" + " and spams emails will be deleted every 3 hours"
+            }
+    
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An error occurred: " + str(e))
+    
+    
 
 # Authenticate a user
 @router.post("/login", response_model=UserResponse)
